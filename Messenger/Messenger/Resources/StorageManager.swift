993a0e3381ef5d79a157a9dbbd72f04472dc235b
Noshaid Ali <noshaid.ali@khaleef.com>
2021-06-03 11:08:25 +0500

    start implementing StorageManager class

---
+++ Messenger/Messenger/Resources/StorageManager.swift
@@ -0,0 +1,51 @@
+//
+//  StorageManager.swift
+//  Messenger
+//
+//  Created by Noshaid Ali on 02/06/2021.
+//
+
+import Foundation
+import FirebaseStorage
+
+public enum StorageErrors: Error {
+    case failedToUpload
+    case failedToGetDownloadUrl
+}
+
+final class StorageManager {
+
+    static let shared = StorageManager()
+    private let storage = Storage.storage().reference()
+
+    public typealias UploadPictureCompletion = (Result<String, Error>) -> Void
+    
+    /*
+     /images/noshaid-gmail-com_profile_picture.png
+     */
+    
+    /// Uploads picture to firebase storage and returns completion with url string to download
+    public func uploadProfilePicture(with data: Data, fileName: String, completion: @escaping UploadPictureCompletion) {
+        
+        storage.child("images/\(fileName)").putData(data, metadata: nil) { (metadata, error) in
+            
+            guard error == nil else {
+                // failed
+                print("failed to upload data to firebase for picture")
+                completion(.failure(StorageErrors.failedToUpload))
+                return
+            }
+            
+            self.storage.child("images/\(fileName)").downloadURL { (url, error) in
+                guard let url = url else {
+                    print("Failed to get download url")
+                    completion(.failure(StorageErrors.failedToGetDownloadUrl))
+                    return
+                }
+                let urlString = url.absoluteString
+                print("download url returned: \(urlString)")
+                completion(.success(urlString))
+            }
+        }
+    }
+}
